2e59ba883b08d024e18e228ea54f43bb9ab6ca1e
soufiane chaoufi <soufiane.chaoufi@gmail.com>
2021-12-12 14:47:49 -0800

    test checkbox functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 function App() {
   const [color, setColor] = React.useState('red')
+  const [checked, setChecked] = React.useState(false)
   const newColor = color === 'red' ? 'blue' : 'red'
 
   function changeColor(e) {
@@ -9,9 +10,19 @@ function App() {
   }
   return (
     <div>
-      <button style={{backgroundColor: color}} onClick={changeColor}>
+      <button
+        style={{backgroundColor: color}}
+        onClick={changeColor}
+        disabled={checked}
+      >
         Change to {newColor}
       </button>
+      <input
+        type="checkbox"
+        id="enable-button-checkbox"
+        aria-checked={checked}
+        onChange={e => setChecked(e.target.checked)}
+      />
     </div>
   )
 }
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -15,3 +15,19 @@ test('button turns blue when clicked', () => {
   expect(button).toHaveStyle({backgroundColor: 'blue'})
   expect(button).toHaveTextContent(/change to red/i)
 })
+
+test('initial condition', () => {
+  render(<App />)
+
+  //check that the button start out enabled
+  const button = screen.getByRole('button', {name: /change to blue/i})
+  expect(button).toBeEnabled()
+
+  // check that the checkout starts  out unchecked
+  const checkbox = screen.getByRole('checkbox')
+  expect(checkbox).not.toBeChecked()
+
+  fireEvent.click(checkbox)
+  expect(button).toBeDisabled()
+  expect(checkbox).toBeChecked()
+})
